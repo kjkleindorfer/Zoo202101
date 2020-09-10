8ff3da8285cd6cfb13764a557b714766b7c0c4e6
GuyDinh <61517623+GuyDinh@users.noreply.github.com>
2020-09-10 11:38:29 -0500

    Add files via upload

---
+++ AnimalNoise.java
@@ -0,0 +1,87 @@
+import model.*;
+
+public class AnimalNoise {
+	//add your animal class to the model package
+	//only push this file and the animal class you created
+
+	public static void main(String[] args) {
+		Cat fluffy = new Cat();
+		System.out.println(fluffy.makeNoise());
+		
+		Dog sparky = new Dog();
+		System.out.println(sparky.makeNoise());
+		
+		Fish goldy = new Fish();
+		System.out.println(goldy.makeNoise());
+		
+		Pig bacon = new Pig();
+		System.out.println(bacon.makeNoise());
+
+		Cow beef = new Cow();
+		System.out.println(beef.makeNoise());
+		
+		Bird roadRunner = new Bird();
+		System.out.println(roadRunner.makeNoise());
+		
+		Goose snow = new Goose();
+		System.out.println(snow.makeNoise());
+		
+		Horse jack = new Horse();
+		System.out.println(jack.makeNoise());
+		
+		Dragon reptar = new Dragon();
+		System.out.println(reptar.makeNoise());
+		
+		Lion king = new Lion(); // Chadwick Burgett
+		System.out.println(king.makeNoise());
+
+		Platypus bob = new Platypus();//Alek Brown
+		System.out.println(bob.makeNoise());
+    
+		//Sheep black = new Sheep(); // Amber Schneider
+		//System.out.println(black.makeNoise());
+
+		Mouse sneaky = new Mouse();
+		System.out.println(sneaky.makeNoise());
+		
+		Donkey Jenny = new Donkey();
+		System.out.println(Jenny.makeNoise());
+    
+		Bear yogi  = new Bear();
+		System.out.println(yogi.makeNoise());
+
+		Monkey bingo = new Monkey();
+		System.out.println(bingo.makeNoise());
+
+		Snake anaconda = new Snake(); 
+		System.out.println(anaconda.makeNoise());
+		
+		Tiger simba = new Tiger();
+		System.out.println(simba.makeNoise());
+		
+		Bat batsy = new Bat();
+		System.out.println(batsy.makeNoise());
+
+		Elephant dumbo = new Elephant();
+		System.out.println(dumbo.makeNoise());
+		
+		Chicken feathers = new Chicken();
+		System.out.println(feathers.makeNoise());
+
+		Llama penny = new Llama();
+		System.out.println(penny.makeNoise());
+
+		Cricket jimminy = new Cricket();
+		System.out.println(jimminy.makeNoise());
+    
+    Mosquito small = new Mosquito();
+		System.out.println(small.makeNoise());
+
+Koala myKoala = new Koala();
+		System.out.println(myKoala.makeNoise());
+
+		Rat splinter = new Rat();
+		System.out.println(splinter.makeNoise());
+	}
+
+}
